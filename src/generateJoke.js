68165353d841131b2ee0ce8460cf5b52425b32a2
Puskás Gellért <devgellert@gmail.com>
2022-03-23 14:12:45 +0100

    initialized based on webpack-starter

---
+++ src/generateJoke.js
@@ -0,0 +1,5 @@
+function generateJoke() {
+    return "asd";
+}
+
+export default generateJoke;
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import generateJoke from "./generateJoke";
+import "./style/main.scss";
+import pic from "./assets/273940987_4721668751249249_7815552545267750745_n.jpg";
+
+
+const img = document.getElementById("img");
+
+img.src = pic;
+
+console.log(pic);
